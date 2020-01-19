34c9a9929a9578ac255b8be4c887b37bcaa19ee1
Anarghya V <30490462+gitofanarghya@users.noreply.github.com>
2020-01-19 17:16:11 +0530

    build

---
+++ build/precache-manifest.f1933792329159ab14bcb161b85969a3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "70c07b7f8dfc0421bb9d13f7efa8703b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d6fb7f09a9964f2eb83f",
+    "url": "/static/css/2.e8a5d6e8.chunk.css"
+  },
+  {
+    "revision": "4dc641ca7cb9f53ec4a8",
+    "url": "/static/css/main.4cd2187f.chunk.css"
+  },
+  {
+    "revision": "d6fb7f09a9964f2eb83f",
+    "url": "/static/js/2.22415e3c.chunk.js"
+  },
+  {
+    "revision": "4dc641ca7cb9f53ec4a8",
+    "url": "/static/js/main.6e5f03d4.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
